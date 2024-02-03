ad71bae4a381b5c9acef65ad8b2c6aaffc60c406
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2024-02-03 12:01:14 +0100

    push

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    //supabaseUrl: 'https://kvjhoifmabxitkelfeod.supabase.co',
+    //supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2amhvaWZtYWJ4aXRrZWxmZW9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMzQ3NDMsImV4cCI6MjAxMjcxMDc0M30.vttAJafQbMLkJFQU0r49cRjKa9fHcmtIQYkcNyAo5A4',
+    appTitle: 'TP (preview)',
+    baseUrl: 'http://localhost:4200/'
+}
