5f23f35433240914eac1ab68d366247c5cf93ee1
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2024-02-05 16:23:42 +0100

    Supabase URLs' Added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,9 +3,10 @@ export const environment = {
   //supabaseUrl: 'https://exspobkugyipwqkqoavo.supabase.co',
   //supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4c3BvYmt1Z3lpcHdxa3FvYXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIzMDQ1NTMsImV4cCI6MjAxNzg4MDU1M30.Wtp6_HOtNkzr9WQN_AGEHnFSYMkjC-m43jV5GP-zQXA',
 
-   // supabaseUrl: 'https://kvjhoifmabxitkelfeod.supabase.co',
-    appTitle: 'TPs (preview)',
-   // supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2amhvaWZtYWJ4aXRrZWxmZW9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMzQ3NDMsImV4cCI6MjAxMjcxMDc0M30.vttAJafQbMLkJFQU0r49cRjKa9fHcmtIQYkcNyAo5A4',
-    baseUrl: 'http://localhost:4200/',
+  supabaseUrl: 'https://dbrlwzqagoixpjkqjbpo.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRicmx3enFhZ29peHBqa3FqYnBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxNDY0NzEsImV4cCI6MjAyMjcyMjQ3MX0.OdPXS4ecXklaWUTmsCotBCJgLm1wdW4RKAxtxaLLEXQ',
+
+  appTitle: 'TPs (preview)',
+  baseUrl: 'http://localhost:4200/',
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    //supabaseUrl: 'https://kvjhoifmabxitkelfeod.supabase.co',
-    //supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2amhvaWZtYWJ4aXRrZWxmZW9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMzQ3NDMsImV4cCI6MjAxMjcxMDc0M30.vttAJafQbMLkJFQU0r49cRjKa9fHcmtIQYkcNyAo5A4',
+    supabaseUrl: 'https://dbrlwzqagoixpjkqjbpo.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRicmx3enFhZ29peHBqa3FqYnBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxNDY0NzEsImV4cCI6MjAyMjcyMjQ3MX0.OdPXS4ecXklaWUTmsCotBCJgLm1wdW4RKAxtxaLLEXQ',
     appTitle: 'TP (preview)',
     baseUrl: 'http://localhost:4200/'
 }
