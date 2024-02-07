3ac8f3cdc2f3d7d4f0fc1d92ec392d03b02f2d63
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2024-02-07 23:02:31 +0100

    Some work on navbar.component.html

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+// navbar.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   logo: string = 'assets/images/logo.png';
+  languages = [
+    { code: 'az', label: 'Azərbaycan', flag: 'path-to-azerbaijan-flag.png' },
+    { code: 'ru', label: 'Русский', flag: 'path-to-russian-flag.png' },
+    { code: 'en', label: 'English', flag: 'path-to-english-flag.png' }
+  ];
+  currentLanguage: string = 'en';
+  showLanguages: boolean = false;
 
+  currentLanguageFlag: string | undefined;
+  currentLanguageLabel: string | undefined;
+
+  constructor() {
+    this.setCurrentLanguage(this.currentLanguage);
+  }
+
+  setCurrentLanguage(code: string) {
+    const language = this.languages.find(lang => lang.code === code);
+    if (language) {
+      this.currentLanguage = code;
+      this.currentLanguageFlag = language.flag;
+      this.currentLanguageLabel = language.label;
+    }
+    this.showLanguages = false;
+  }
 }
