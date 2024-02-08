f20fa91e9236cee95884d89ffe27110ca8801818
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2024-02-08 17:09:15 +0100

    header/navbar v1 finished

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -9,15 +9,18 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   logo: string = 'assets/images/logo.png';
   languages = [
-    { code: 'az', label: 'Azərbaycan', flag: 'path-to-azerbaijan-flag.png' },
-    { code: 'ru', label: 'Русский', flag: 'path-to-russian-flag.png' },
-    { code: 'en', label: 'English', flag: 'path-to-english-flag.png' }
+    { code: 'az', label: 'Azərbaycan', flag: 'assets/images/flags/az.png' },
+    { code: 'ru', label: 'Русский', flag: 'assets/images/flags/ru.png' },
+    { code: 'en', label: 'English', flag: 'assets/images/flags/en.png' }
   ];
   currentLanguage: string = 'en';
   showLanguages: boolean = false;
 
   currentLanguageFlag: string | undefined;
   currentLanguageLabel: string | undefined;
+  showServicesDropdown: boolean | undefined;
+  showAboutDropdown: boolean | undefined;
+  showMenu: boolean | undefined;
 
   constructor() {
     this.setCurrentLanguage(this.currentLanguage);
@@ -31,5 +34,33 @@ export class NavbarComponent {
       this.currentLanguageLabel = language.label;
     }
     this.showLanguages = false;
+  }
+
+  showServices() {
+    this.showServicesDropdown = !this.showServicesDropdown;
+    this.showMenu = false;
+    this.showAboutDropdown = false;
+    this.showLanguages = false;
+  }
+
+  showAbout() {
+    this.showAboutDropdown = !this.showAboutDropdown;
+    this.showMenu = false;
+    this.showServicesDropdown = false;
+    this.showLanguages = false;
+  }
+
+  showMobileMenu() {
+    this.showMenu = !this.showMenu;
+    this.showServicesDropdown = false;
+    this.showAboutDropdown = false;
+    this.showLanguages = false;
+  }
+
+  showLanguage() {
+    this.showLanguages = !this.showLanguages;
+    this.showMenu = false;
+    this.showServicesDropdown = false;
+    this.showAboutDropdown = false;
   }
 }
