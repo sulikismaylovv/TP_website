445cbb9425797e93de85da13805f33d4d52bd006
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2024-02-10 00:00:02 +0100

    multilanguage started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import {NgOptimizedImage} from "@angular/common";
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import {NgOptimizedImage} from "@angular/common";
   ],
     imports: [
         BrowserModule,
-        NgOptimizedImage
+        NgOptimizedImage,
+        HttpClientModule,
+        TranslocoRootModule
     ],
   providers: [],
   bootstrap: [AppComponent]
